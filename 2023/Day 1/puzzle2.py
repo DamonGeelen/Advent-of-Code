eca5fdbc1a2815bfde2fff27fe035a2c99e4181c
Damon Geelen <damonrobertgeelen@gmail.com>
2023-12-04 00:35:10 -0500

    Day 1 Puzzle 2 Solved

---
+++ 2023/Day 1/puzzle2.py
@@ -0,0 +1,36 @@
+with open("input.txt", "r") as file:
+    content = file.read()
+
+content = content.replace('zero', 'z0o')
+content = content.replace('one', 'o1e')
+content = content.replace('two', 't2o')
+content = content.replace('three', 't3e')
+content = content.replace('four', 'f4r')
+content = content.replace('five', 'f5e')
+content = content.replace('six', 's6x')
+content = content.replace('seven', 's7n')
+content = content.replace('eight', 'e8t')
+content = content.replace('nine', 'n9e')
+
+sum = 0
+first_number = last_number = -1
+
+for char in content:
+
+    if char.isnumeric():
+
+        if first_number == -1:
+            first_number = last_number = char
+
+        else:
+            last_number = char
+        
+    if char == "\n":
+
+        if first_number == -1 or last_number == -1:
+            print("Error in detecting first or last number in line.")
+
+        sum += int(first_number + last_number)
+        first_number = last_number = -1
+
+print(f"The sum of all of the calibration values is {sum}.")
